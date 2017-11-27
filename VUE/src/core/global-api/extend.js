776229a79ad2121905cc7cf942b3a6bfbee81076
廖飞银 <15196004361@163.com>
2017-11-27 09:55:26 +0800

    Vue.extend

--- VUE/src/core/global-api/extend.js
+++ VUE/src/core/global-api/extend.js
@@ -10,17 +10,21 @@ export function initExtend (Vue: GlobalAPI) {
    * cid. This enables us to create wrapped "child
    * constructors" for prototypal inheritance and cache them.
    */
+  // 包括 Vue 在内的每个实例构造函数都有一个独立的 cid。
+  // 这个cid能够让我们创建一个用于原型继承的包裹的子构造函数并进行缓存。
   Vue.cid = 0
   let cid = 1
 
-  /**
-   * Class inheritance
-   */
+  // 类继承
   Vue.extend = function (extendOptions: Object): Function {
     extendOptions = extendOptions || {}
     const Super = this
+
+    // 用于缓存子构造函数
     const SuperId = Super.cid
     const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {})
+
+    // 如果有缓存的构造函数，直接返回
     if (cachedCtors[SuperId]) {
       return cachedCtors[SuperId]
     }
@@ -36,11 +40,18 @@ export function initExtend (Vue: GlobalAPI) {
       }
     }
 
+    // Sub（是一个子类构造方法）用于初始化一个 Vue 组件
     const Sub = function VueComponent (options) {
       this._init(options)
     }
+    
+    // 原型继承
+    // Sub 继承于 Super（Super === Vue）
     Sub.prototype = Object.create(Super.prototype)
+    // 直接赋值的方式让 Sub 的 constructor 指向了 Super，所以这里要重写
     Sub.prototype.constructor = Sub
+
+    // 添加静态属性
     Sub.cid = cid++
     Sub.options = mergeOptions(
       Super.options,
@@ -51,6 +62,8 @@ export function initExtend (Vue: GlobalAPI) {
     // For props and computed properties, we define the proxy getters on
     // the Vue instances at extension time, on the extended prototype. This
     // avoids Object.defineProperty calls for each instance created.
+    // 对于 props 和 computed 属性，我们在Vue实例扩展时为继承的原型定义了代理getters
+    // 这避免了为创建的每一个实例调用 Object.defineProperty
     if (Sub.options.props) {
       initProps(Sub)
     }
@@ -59,16 +72,19 @@ export function initExtend (Vue: GlobalAPI) {
     }
 
     // allow further extension/mixin/plugin usage
+    // 允许进一步扩展、混合、插件的使用
     Sub.extend = Super.extend
     Sub.mixin = Super.mixin
     Sub.use = Super.use
 
     // create asset registers, so extended classes
     // can have their private assets too.
+    // 创建 asset 注册器，让每一个扩展类也有自己的私有 asset
     ASSET_TYPES.forEach(function (type) {
       Sub[type] = Super[type]
     })
     // enable recursive self-lookup
+    // 能够递归自我查找
     if (name) {
       Sub.options.components[name] = Sub
     }
